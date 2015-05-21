f7ba4e4a088849f4678e6c45f205014f809280d7
weicheng xi <xiwc87@yeah.net>
2015-05-21 19:34:55 +0800

    ...

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,17 @@ jQuery(document).ready(function($) {
 	//$.test.foo1();
 	//$.test.bar1('param');
 
-	$('.hilight').hilight()
+	$('.hilight').hilight();
+
+var $img1 = $('.c1');
+var $img2 = $('.c2');
+
+$img2.each(function(index, el) {
+	$(el).attr({
+		'th:src': $($img1.get(index)).attr('th:src')
+	});
+});
+
+console.log($img2.appendTo("#d").html());
+
 });
